47713a0d4b87bd8f0df2fdc19383b74d38f5febc
firsttris <tristanteufel@googlemail.com>
2020-02-28 21:17:53 +0100

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  moduleDirectories: ['node_modules', 'packages/common', 'packages/app/src'],
+  moduleDirectories: ['node_modules', 'packages', 'packages/app/src'],
   collectCoverageFrom: ['**/server/src/**'],
   preset: 'ts-jest'
 };
